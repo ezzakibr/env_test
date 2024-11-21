9b5e5d6d5b6a09913ece840c044e625b3ac31aaa
ezzakibr <brahim.ezzaki@outlook.fr>
2024-11-21 12:29:34 +0100

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,14 +1,17 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, from_unixtime, when
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
 
 
 spark = SparkSession.builder \
     .appName("KafkaCassandraPipeline") \
-    .config("spark.jars.packages", 
-            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,"
+    .config("spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0," +
             "com.datastax.spark:spark-cassandra-connector_2.12:3.2.0") \
+    .config("spark.cassandra.connection.host", "localhost") \
+    .config("spark.cassandra.connection.port", "9042") \
     .getOrCreate()
+
 
 schema = StructType([
     StructField("id", IntegerType()),
@@ -23,20 +26,44 @@ df = spark.readStream \
     .option("subscribe", "test_topic") \
     .load()
 
-
 parsed_df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
 
-
-processed_df = parsed_df \
-    .withColumn("value", col("value") * 2)  
-
-
-query = processed_df.writeStream \
+categories_df = spark.read \
     .format("org.apache.spark.sql.cassandra") \
-    .option("checkpointLocation", "/tmp/checkpoint") \
-    .option("keyspace", "test_keyspace") \
-    .option("table", "messages") \
-    .start()
+    .options(table="categories", keyspace ="test_keyspace") \
+    .load()
 
+
+
+
+def process_batch(batch_df, batch_id):
+    if batch_df.count() > 0:
+        processed = batch_df \
+            .join(categories_df, batch_df.category == categories_df.code, "left") \
+            .select(
+                batch_df.id,
+                from_unixtime(batch_df.timestamp).cast("timestamp").alias("date"),
+                batch_df.value,
+                batch_df.category,
+                categories_df.name.alias("category_name")
+            )
+        
+        print(f"\nProcessing batch {batch_id}:")
+        processed.show()
+        
+        processed.write \
+            .format("org.apache.spark.sql.cassandra") \
+            .option("keyspace", "test_keyspace") \
+            .option("table", "messages_with_categories") \
+            .mode("append") \
+            .save()
+        print(f"Batch {batch_id} successfully written to Cassandra")
+
+
+query = parsed_df.writeStream \
+    .foreachBatch(process_batch) \
+    .outputMode("append") \
+    .option("checkpointLocation", "/tmp/checkpoint_messages_with_categories") \
+    .start()
 
 query.awaitTermination()
